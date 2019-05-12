70781b577577ae9748c5eb59b3c9e082b1d9991c
vomelchenko <v.omelchenko@smartweb.com.ua>
2019-05-12 17:37:09 +0300

    use open ssl

--- rust-actix/src/main.rs
+++ rust-actix/src/main.rs
@@ -1,21 +1,20 @@
 use std::fs::File;
 use std::io::BufReader;
 
+use std::fs;
 use bytes::Bytes;
 use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
-use rustls::internal::pemfile::{certs, rsa_private_keys};
-use rustls::{NoClientAuth, ServerConfig};
-use std::fs;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 
 fn main() -> std::io::Result<()> {
-    // load ssl keys
-    let mut config = ServerConfig::new(NoClientAuth::new());
-    let cert_file = &mut BufReader::new(&include_bytes!("cert.pem")[..]);
-    let key_file = &mut BufReader::new(&include_bytes!("key.pem")[..]);
-    let cert_chain = certs(cert_file).unwrap();
-    let mut keys = rsa_private_keys(key_file).unwrap();
-    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
+    // Create the TLS acceptor.
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("/etc/key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("/etc/cert.pem").unwrap();
+
 
     HttpServer::new(|| {
         App::new()
@@ -25,8 +24,8 @@ fn main() -> std::io::Result<()> {
                     .body(Bytes::from(fs::read("/data/med.txt").unwrap()))
             }))
     })
-    .workers(1)
+    .workers(4)
     .disable_signals()
-    .bind_rustls("0.0.0.0:443", config)?
+    .bind_ssl("0.0.0.0:443", builder)?
     .run()
 }
