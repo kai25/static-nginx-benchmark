f5e79ffa4226626de1240e6262fd73d1248889dc
vomelchenko <v.omelchenko@smartweb.com.ua>
2019-05-12 16:43:57 +0300

    workers = 2

--- rust-actix/src/main.rs
+++ rust-actix/src/main.rs
@@ -17,18 +17,15 @@ fn main() -> std::io::Result<()> {
     let mut keys = rsa_private_keys(key_file).unwrap();
     config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
 
-    let file_data = Bytes::from(fs::read("/data/med.txt").unwrap());
-
-    HttpServer::new(move || {
-        let file_data = file_data.clone();
+    HttpServer::new(|| {
         App::new()
-            .service(web::resource("/med.txt").to(move |req: HttpRequest| {
+            .service(web::resource("/med.txt").to(|req: HttpRequest| {
                 HttpResponse::Ok()
                     .content_type("text/plain")
-                    .body(file_data.clone())
+                    .body(Bytes::from(fs::read("/data/med.txt").unwrap()))
             }))
     })
-    .workers(4)
+    .workers(2)
     .bind_rustls("0.0.0.0:443", config)?
     .run()
 }
