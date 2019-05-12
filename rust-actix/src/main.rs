a32680aa3789f04c9ffcfc44d4beca3ba4ce0464
vomelchenko <v.omelchenko@smartweb.com.ua>
2019-05-12 16:15:47 +0300

    rust actix tls

--- rust-actix/src/main.rs
+++ rust-actix/src/main.rs
@@ -1,22 +1,40 @@
-extern crate actix_web;
+use std::fs::File;
+use std::io::BufReader;
 
 use bytes::Bytes;
-use actix_web::{server, App, HttpRequest, Responder, HttpResponse};
-use actix_web::http::{Method, ContentEncoding};
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use rustls::internal::pemfile::{certs, rsa_private_keys};
+use rustls::{NoClientAuth, ServerConfig};
 use std::fs;
 
-fn index(req: &HttpRequest) -> HttpResponse {
-    HttpResponse::Ok()
-        // v- disable compression
-        .content_encoding(ContentEncoding::Identity)
-        .body(Bytes::from(fs::read("/data/med.txt").unwrap()))
+
+fn index(req: HttpRequest) -> Result<HttpResponse, Error> {
+    println!("{:?}", req);
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain")
+        .body("Welcome!"))
 }
 
-fn main() {
-    server::new(|| App::new()
-        .resource("/med.txt",
-                  |r| r.method(Method::GET).f(index)))
-        .bind("0.0.0.0:80")
-        .unwrap()
-        .run();
+fn main() -> std::io::Result<()> {
+    // load ssl keys
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    let cert_file = &mut BufReader::new(File::open("cert.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("key.pem").unwrap());
+    let cert_chain = certs(cert_file).unwrap();
+    let mut keys = rsa_private_keys(key_file).unwrap();
+    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
+
+    let file_data = Bytes::from(fs::read("/data/med.txt").unwrap());
+
+    HttpServer::new(move || {
+        let file_data = file_data.clone();
+        App::new()
+            .service(web::resource("/med.txt").to(move |req: HttpRequest| {
+                HttpResponse::Ok()
+                    .content_type("text/plain")
+                    .body(file_data.clone())
+            }))
+    })
+    .bind_rustls("0.0.0.0:443", config)?
+    .run()
 }
